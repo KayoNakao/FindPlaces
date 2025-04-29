125f950ec610a55f5592df6de518aa1770d298bb
Kayo Nakao <kayo.nakao87@gmail.com>
2025-04-29 18:26:25 -0400

    Add Detail View Controller

---
+++ FindPlaces/ViewControllers/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  FindPlaces
+//
+//  Created by Kayo on 2025-04-29.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
